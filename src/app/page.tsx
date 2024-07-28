978b5c3d62cbcf4b0bd62d6da537429dbb849466
Saikat <saikat@Saikats-MacBook-Pro.local>
2024-07-28 06:19:46 +0600

    Create simple route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,20 @@
+import { Button } from "@nextui-org/react";
+import Link from "next/link";
+import { FaRegSmile } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <div className="text-3xl">Hello app!</div>
+    <div>
+      <h1 className="text-3xl">Hello app!</h1>
+      <Button
+        as={Link}
+        href="/members"
+        color="primary"
+        variant="bordered"
+        startContent={<FaRegSmile size={20} />}
+      >
+        Members page
+      </Button>
+    </div>
   );
 }
