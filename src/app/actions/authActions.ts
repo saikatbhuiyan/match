ac2d2e265498eebfd57f7347d42ca15e7ea62276
Saikat <saikat@Saikats-MacBook-Pro.local>
2024-08-08 23:17:58 +0600

    Create login actions

--- src/app/actions/authActions.ts
+++ src/app/actions/authActions.ts
@@ -5,6 +5,36 @@ import { RegisterSchema, registerSchema } from "@/lib/schemas/registerSchema";
 import { prisma } from "@/lib/prisma";
 import { ActionResult } from "@/types";
 import { User } from "@prisma/client";
+import { AuthError } from "next-auth";
+import { signIn } from "@/auth";
+import { LoginSchema } from "@/lib/schemas/loginSchema";
+
+export async function signInUser(
+  data: LoginSchema
+): Promise<ActionResult<string>> {
+  try {
+    const result = await signIn("credentials", {
+      email: data.email,
+      password: data.password,
+      redirect: false,
+    });
+    console.log(result);
+
+    return { status: "success", data: "Logged in" };
+  } catch (error) {
+    console.log(error);
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return { status: "error", error: "Invalid credentials" };
+        default:
+          return { status: "error", error: "Something went wrong" };
+      }
+    } else {
+      return { status: "error", error: "Something else went wrong" };
+    }
+  }
+}
 
 export async function registerUser(
   data: RegisterSchema
@@ -20,9 +50,7 @@ export async function registerUser(
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const existingUser = await prisma.user.findUnique({
-      where: { email: email },
-    });
+    const existingUser = await getUserByEmail(email);
 
     if (existingUser) return { status: "error", error: "User already exists" };
 
--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,11 +1,18 @@
-'use client'
+"use client";
 
 import React, { ReactNode } from "react";
 import { NextUIProvider } from "@nextui-org/react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Providers({ children }: { children: ReactNode }) {
-  return( 
+  return (
     <NextUIProvider>
+      <ToastContainer
+        position="bottom-right"
+        hideProgressBar
+        className="z-50"
+      />
       {children}
     </NextUIProvider>
   );
