cd3dcceb714a1b96bcb6e14ab348482f178d61b7
Saikat <saikat@Saikats-MacBook-Pro.local>
2024-08-12 05:53:10 +0600

    UserMenu and logouut

--- src/components/navbar/TopNav.tsx
+++ src/components/navbar/TopNav.tsx
@@ -1,23 +1,22 @@
-import {
-  Button,
-  Navbar,
-  NavbarBrand,
-  NavbarContent,
-  NavbarItem,
-} from "@nextui-org/react";
+import { Button, Navbar, NavbarBrand, NavbarContent } from "@nextui-org/react";
 import Link from "next/link";
 import React from "react";
 import { GiMatchTip } from "react-icons/gi";
 import NavLink from "./NavLink";
+import { auth } from "@/auth";
+import UserMenu from "./UserMenu";
 
 export default async function TopNav() {
+  const session = await auth();
+  const userInfo = session?.user || null;
+
   const memberLinks = [
     { href: "/members", label: "Matches" },
     { href: "/lists", label: "Lists" },
     { href: "/messages", label: "Messages" },
   ];
 
-  const adminLinks = [{ href: "/admin/moderation", label: "Photo Moderation" }];
+  const links = memberLinks;
 
   return (
     <>
@@ -36,35 +35,39 @@ export default async function TopNav() {
         <NavbarBrand as={Link} href="/">
           <GiMatchTip size={40} className="text-gray-200" />
           <div className="font-bold text-3xl flex">
-            <span className="text-gray-900">My</span>
+            <span className="text-gray-900">Next</span>
             <span className="text-gray-200">Match</span>
           </div>
         </NavbarBrand>
         <NavbarContent justify="center">
-          {memberLinks &&
-            memberLinks.map((item) => (
+          {session &&
+            links.map((item) => (
               <NavLink key={item.href} href={item.href} label={item.label} />
             ))}
         </NavbarContent>
         <NavbarContent justify="end">
-          <>
-            <Button
-              as={Link}
-              href="/login"
-              variant="bordered"
-              className="text-white"
-            >
-              Login
-            </Button>
-            <Button
-              as={Link}
-              href="/register"
-              variant="bordered"
-              className="text-white"
-            >
-              Register
-            </Button>
-          </>
+          {userInfo ? (
+            <UserMenu userInfo={userInfo} />
+          ) : (
+            <>
+              <Button
+                as={Link}
+                href="/login"
+                variant="bordered"
+                className="text-white"
+              >
+                Login
+              </Button>
+              <Button
+                as={Link}
+                href="/register"
+                variant="bordered"
+                className="text-white"
+              >
+                Register
+              </Button>
+            </>
+          )}
         </NavbarContent>
       </Navbar>
     </>
